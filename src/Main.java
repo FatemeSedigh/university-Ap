3df3b43da45996763adb6ba7d2d115b01baa16dc
fati <fatemesadatsedigh@gmail.com>
2025-03-20 15:54:10 +0330

    add *.iml

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Let's go to enter the university code.!");
+        System.out.println("Let's go to enter the university code!");
     }
 }
