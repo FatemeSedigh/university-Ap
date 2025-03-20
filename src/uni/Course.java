cf2296e6ad676c0af8201cbed6c39391be55a931
fati <fatemesadatsedigh@gmail.com>
2025-03-20 19:27:29 +0330

    commit -Fields-and-Methods-Course

--- src/uni/Course.java
+++ src/uni/Course.java
@@ -1,4 +1,27 @@
 package uni;
 
+import java.util.ArrayList;
+
 public class Course {
+
+    public int id;
+    public static ArrayList<Course> courseList = new ArrayList<>();
+    public String title;
+    public int units;
+
+    public Course(String title, int units) {
+        this.title = title;
+        this.units = units;
+        courseList.add(this);
+        this.id = courseList.size();
+    }
+
+    public static Course findByID(int id) {
+        for (Course findCourse : courseList) {
+            if (findCourse.id == id) {
+                return findCourse;
+            }
+        }
+        return null;
+    }
 }
