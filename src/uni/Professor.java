8a207df5ef8a92d698a7eebf4748718dc8607428
fati <fatemesadatsedigh@gmail.com>
2025-03-20 17:29:45 +0330

    commit Fields-and-Methods-Professor

--- src/uni/Professor.java
+++ src/uni/Professor.java
@@ -1,4 +1,27 @@
 package uni;
 
+import java.util.ArrayList;
+
 public class Professor {
+
+    public int id;
+    public static ArrayList<Professor> professorList = new ArrayList<>();
+    public int personID;
+    public int majorID;
+
+    public Professor(int personID, int majorID) {
+        this.personID = personID;
+        this.majorID = majorID;
+        professorList.add(this);
+        this.id = professorList.size();
+    }
+
+    public static Professor findByID(int id) {
+        for (Professor findProfessor : professorList) {
+            if (findProfessor.id == id) {
+                return findProfessor;
+            }
+        }
+        return null;
+    }
 }
