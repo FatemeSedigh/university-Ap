927f4ae6eb8d164c0b02117f01ba1446071252eb
fati <fatemesadatsedigh@gmail.com>
2025-03-20 17:24:25 +0330

    commit Fields-and-Methods-Student

--- src/uni/Student.java
+++ src/uni/Student.java
@@ -1,4 +1,35 @@
 package uni;
 
+import java.util.ArrayList;
+
 public class Student {
+
+    public int id;
+    public static ArrayList<Student> studentList = new ArrayList<>();
+    public int personID;
+    public final int entranceYear;
+    public int majorID;
+    public String studentID;
+
+    public Student(int personID, int entranceYear, int majorID) {
+        this.personID = personID;
+        this.entranceYear = entranceYear;
+        this.majorID = majorID;
+        studentList.add(this);
+        this.id = studentList.size();
+        this.setStudentCode();
+    }
+
+    public static Student findByID(int id) {
+        for (Student findStudent : studentList) {
+            if (findStudent.id == id) {
+                return findStudent;
+            }
+        }
+        return null;
+    }
+
+    public void setStudentCode() {
+        this.studentID = this.entranceYear + String.format("%04d", this.id);
+    }
 }
