b13858230ea68a14028b6629c7fbfcd520f16571
fati <fatemesadatsedigh@gmail.com>
2025-03-20 16:33:46 +0330

    mommit writing-Fields-and-Methods-Person

--- src/base/Person.java
+++ src/base/Person.java
@@ -1,4 +1,27 @@
 package base;
+import java.util.*;
 
 public class Person {
+
+    public int id;
+    public static ArrayList<Person> personList = new ArrayList<>();
+    public String name;
+    public final String nationalID;
+
+    public Person(String name, String nationalID){
+        this.name = name;
+        this.nationalID = nationalID;
+        personList.add(this);
+        id = personList.size();
+    }
+
+    public static Person findByID(int id){
+        for(Person findPerson : personList){
+            if(findPerson.id == id){
+                return findPerson;
+            }
+        }
+        return null;
+    }
+
 }
